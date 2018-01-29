c7a24d8e60fb1af7c079ce7cb74ffe17bc133a29
Chapa.C(Takuya) <chapatown@gmail.com>
2018-01-29 22:43:24 +0900

    スクロールビューの準備

---
+++ Assets/Scripts/UI/GhostListCell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+namespace Ghost
+{
+	public class GhostListCell : MonoBehaviour
+	{
+		[SerializeField]
+		private Image _ghostImage;
+
+		[SerializeField]
+		private Image[] _arrowImages = new Image[10];
+
+		public void SetArrowsEnable(int[] enables)
+		{
+			Array.ForEach(_arrowImages, image => image.enabled = false);
+			Array.ForEach(enables, num => _arrowImages[num].enabled = true);
+		}
+	}
+}
